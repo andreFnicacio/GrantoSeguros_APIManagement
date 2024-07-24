a687811905c371c32d6bcea3f0590b643e2af5fa
andreFnicacio <andrenicaciodev@gmail.com>
2024-07-24 02:35:37 -0300

    fixed stuffs

--- src/swagger.ts
+++ src/swagger.ts
@@ -12,7 +12,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: 'https://grantosegurosapimanagement-production.up.railway.app',
       },
     ],
   },
